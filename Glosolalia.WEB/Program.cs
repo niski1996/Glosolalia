cd6baf1048814b14452f9f69874515c3bee2ad09
Karol <niski1996@interia.pl>
2023-05-19 00:33:11 +0200

    trai rebase

--- Glosolalia.WEB/Program.cs
+++ Glosolalia.WEB/Program.cs
@@ -1,10 +1,16 @@
 using Glosolalia.Data;
+using Glosolalia.Data.Data_MockRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ITranslationRepository, MockTranslationRepository>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
 
 app.Run();
