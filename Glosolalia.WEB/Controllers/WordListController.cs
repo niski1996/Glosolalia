1cae014326187290a043bbd842630526b2fd527a
Karol <niski1996@interia.pl>
2023-05-21 00:58:53 +0200

    s

---
+++ Glosolalia.WEB/Controllers/WordListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Glosolalia.WEB.Controllers
+{
+	public class WordListController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
